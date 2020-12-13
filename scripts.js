63405e8651afce3170818127f381fa4373daf318
ayiamco <josephachonu@gmail.com>
2020-12-13 17:18:24 +0100

    feature: used local storage to store user preference

--- scripts.js
+++ scripts.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.querySelector("#theme-options-wrapper");
   let root = document.documentElement;
+  let themeColor= window.localStorage.getItem("themeColor")
+  const lightMode="light-mode";
+  const darkMode="blue-mode";
+  if(!themeColor){ 
+    setWhiteMode()
+    console.log("hjfkhjlhjlk")
+  }
+  else{
+    console.log(themeColor)
+    themeColor=== lightMode ? setWhiteMode(): setDarkMode();
+  };
 
-  button.addEventListener("click", function (e) {
-    if (e.target.id === "blue-mode") {
-      root.style.setProperty("--mainColor", "#15202b");
-      root.style.setProperty("--secondaryColor", "#192734");
-      root.style.setProperty("--secondaryText", "#eeeeee");
-      root.style.setProperty("--borderColor", "#164d56");
-      root.style.setProperty("--mainText", "#fff");
-      root.style.setProperty("--themeDotBorder", "#fff");
-      root.style.setProperty("--previewBg", "rgb(25, 39, 52, 0.8)");
-      root.style.setProperty("--previewShadow", "#111921");
-      root.style.setProperty("--buttonColor", "#17a2b8");
-    }
-    if (e.target.id == "light-mode") {
+  function setWhiteMode(){
       root.style.setProperty("--mainColor", "#eaeaea");
       root.style.setProperty("--secondaryColor", "#fff");
       root.style.setProperty("--secondaryText", "#4b5156");
@@ -24,6 +23,28 @@ document.addEventListener("DOMContentLoaded", function () {
       root.style.setProperty("--previewBg", "rgb(251, 249, 243, 0.8)");
       root.style.setProperty("--previewShadow", "#f0ead6");
       root.style.setProperty("--buttonColor", "#000");
+  }
+
+  function setDarkMode(){
+    root.style.setProperty("--mainColor", "#15202b");
+      root.style.setProperty("--secondaryColor", "#192734");
+      root.style.setProperty("--secondaryText", "#eeeeee");
+      root.style.setProperty("--borderColor", "#164d56");
+      root.style.setProperty("--mainText", "#fff");
+      root.style.setProperty("--themeDotBorder", "#fff");
+      root.style.setProperty("--previewBg", "rgb(25, 39, 52, 0.8)");
+      root.style.setProperty("--previewShadow", "#111921");
+      root.style.setProperty("--buttonColor", "#17a2b8");
+      return;
+  }
+  button.addEventListener("click", function (e) {
+    if (e.target.id === darkMode) {
+      setDarkMode()
+      window.localStorage.setItem("themeColor",darkMode)
+    }
+    if (e.target.id == "light-mode") {
+       setWhiteMode()
+      window.localStorage.setItem("themeColor",lightMode)
     }
   });
 
@@ -40,7 +61,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (e.target.id === "dot-1") {
       // tab.classList.toggle("hide-display");
     window.close()
-    
     }
   });
 });
