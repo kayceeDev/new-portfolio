d8064b8b473613f6131d53236c22f501109719b7
kayceedev <Nwobodoe71@gmail.com>
2020-12-13 14:36:55 +0100

    portfolio project initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Porfolio Project
+
+This project showcases most of the porjects that i have done which i am proud of
+
+It also contains my contacts incase you want to reach out to me anytime
---
+++ scripts.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("#theme-options-wrapper");
+  let root = document.documentElement;
+
+  button.addEventListener("click", function (e) {
+    if (e.target.id === "blue-mode") {
+      root.style.setProperty("--mainColor", "#15202b");
+      root.style.setProperty("--secondaryColor", "#192734");
+      root.style.setProperty("--secondaryText", "#eeeeee");
+      root.style.setProperty("--borderColor", "#164d56");
+      root.style.setProperty("--mainText", "#fff");
+      root.style.setProperty("--themeDotBorder", "#fff");
+      root.style.setProperty("--previewBg", "rgb(25, 39, 52, 0.8)");
+      root.style.setProperty("--previewShadow", "#111921");
+      root.style.setProperty("--buttonColor", "#17a2b8");
+    }
+    if (e.target.id == "light-mode") {
+      root.style.setProperty("--mainColor", "#eaeaea");
+      root.style.setProperty("--secondaryColor", "#fff");
+      root.style.setProperty("--secondaryText", "#4b5156");
+      root.style.setProperty("--borderColor", "#c1c1c1");
+      root.style.setProperty("--mainText", "#000");
+      root.style.setProperty("--themeDotBorder", "#24292e");
+      root.style.setProperty("--previewBg", "rgb(251, 249, 243, 0.8)");
+      root.style.setProperty("--previewShadow", "#f0ead6");
+      root.style.setProperty("--buttonColor", "#000");
+    }
+  });
+
+  let leftCol = document.querySelector(".left-column");
+  let rightCol = document.querySelector(".right-column");
+  let tab = document.querySelector(".intro-wrapper");
+  const minimize = document.querySelector(".dots-wrapper");
+
+  minimize.addEventListener("click", function (e) {
+    if (e.target.id === "dot-2") {
+      leftCol.classList.toggle("hide-display");
+      rightCol.classList.toggle("hide-display");
+    }
+    if (e.target.id === "dot-1") {
+      tab.classList.toggle("hide-display");
+    }
+  });
+});
